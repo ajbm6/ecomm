4318cee4ac043799ef5f33d49060487330577906
Ranie Santos <ranietots32@gmail.com>
2014-11-06 14:10:47 +0800

    added starting resource files, prepared blade layout

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'forge',
+			'database'  => 'ecomm',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
