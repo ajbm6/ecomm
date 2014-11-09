d3e49bcee4efe8fd845f307744cd97153a5ab368
Ranie Santos <ranietots32@gmail.com>
2014-11-09 16:28:31 +0800

    (tutorial pt2) created eloquent relationships between Product and Category models, ProductsController not yet done

--- app/models/Category.php
+++ app/models/Category.php
@@ -5,4 +5,8 @@ class Category extends Eloquent {
 	protected $fillable = ['name'];
 
 	public static $rules = ['name' => 'required|min:3'];
+
+	public function products(){
+		return $this->hasMany('Product');
+	}
 }
