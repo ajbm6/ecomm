11b45a2d03ee97ff32fb2c249080f9a625ab9f8f
Ranie Santos <ranietots32@gmail.com>
2015-01-27 01:11:22 +0800

    changed company email

--- app/libs/Company.php
+++ app/libs/Company.php
@@ -3,7 +3,7 @@
 class Company {
 
 	public static $name		= 'TechStax';
-	public static $email	= 'contact@techstax.com';
+	public static $email	= 'techstax@yahoo.com';
 	public static $phone	= '09091234567';
 	public static $desc		= 'Lorem ipsum dolor sit amet, consectetur.';
 	public static $address	= '80 Shaw Boulevard,<br>Mandaluyong, 1552 Metro Manila, Philippines';
